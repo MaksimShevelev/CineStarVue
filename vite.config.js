03989cdba12b4b5c035799afeccded29f46e50ac
Maxim <m.v.shevelev1991@gmail.com>
2024-08-07 00:32:39 +0300

    PWA

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,61 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  base: './',
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest: {
+        name: 'CineStar',
+        short_name: 'CineStar',
+        description: 'A Vue.js PWA for movie lovers',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'src/assets/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'src/assets/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          }
+        ],
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/cdn\.jsdelivr\.net\/.*/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'jsdelivr-cdn',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /^https:\/\/cdn\.example\.com\/.*/,
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'example-cdn',
+              expiration: {
+                maxEntries: 10,
+                maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
 })
